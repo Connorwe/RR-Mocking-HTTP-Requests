fbedd984631dbcd837e6586bcf1cd18dbe03a1a9
Connor <connor5741213@gmail.com>
2022-11-05 13:37:39 -0400

    finished github page with react

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,25 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
+
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Connor Edwards'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('Connor Edwards')
+  })
+
+  test("receives GitHub URL", async () => {
+    fetch.mockResponseOnce(JSON.stringify({html_url: 'https://github.com/Connorwe'}))
+    render(<App />)
+    const gitHubURL = await waitFor(() => screen.getByRole('link'))
+    expect(gitHubURL).toHaveAttribute('href', expect.stringContaining('github.com'))
+  })
+
 
 
 
